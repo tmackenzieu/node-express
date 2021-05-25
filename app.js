038e3fe3dc37648e80d4362f5e9b751c2d261974
Tabata G Mackenzie U <tmackenzie@sodimac.cl>
2021-05-25 18:39:10 -0400

    add delete functionality

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser')
 
 require('dotenv').config()
 
 const app = express();
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
 
 const port = process.env.PORT || 3000;
 
--- routing/Mascotas.js
+++ routing/Mascotas.js
@@ -15,4 +15,65 @@ router.get('/', async(req, res) => {
     }
 })
 
+router.get('/crear', (req,res) => {
+    res.render('create')
+})
+
+router.post('/', async(req,res) => {
+    const body = req.body
+    try {
+        await mascota.create(body);
+        res.redirect('/mascotas')
+    } catch (error) {
+        console.log(error)
+    }
+  
+})
+
+router.get('/:id', async(req,res) => {
+    const id = req.params.id;
+    try {
+        const mascotaById = await mascota.findOne({_id: id});
+
+        res.render('detalle', {
+            mascota: mascotaById,
+            error: false
+        })
+
+        console.log(mascotaById)
+
+    } catch (error) {
+        console.log(error)
+        res.render('detalle', {
+            error: true,
+            mensaje: 'No se encuentra el id seleccionado'
+        })
+    }
+})
+
+router.delete('/:id', async(req,res) => {
+    const id = req.params.id;
+    try {
+        const mascotaById = await mascota.findByIdAndDelete({_id: id});
+        if(mascotaById){
+            res.json({
+                estado: true, 
+                mensaje: 'eliminado!'
+            })
+        }else{
+            res.json({
+                estado: false, 
+                mensaje: 'falló la eliminación!'
+            })
+        }
+
+    } catch (error) {
+        console.log(error)
+        res.render('detalle', {
+            error: true,
+            mensaje: 'No se encuentra el id seleccionado'
+        })
+    }
+})
+
 module.exports = router;
